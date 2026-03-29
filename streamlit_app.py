924b9e7ce9baad64c5b0becbdf82fae61a3d822a
MohamedMorgan10 <engmohamedmorgan10@gmail.com>
2026-03-29 10:32:20 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 import streamlit as st
-st.title("Modenr AI Dashboard
-Mohamed Morgan Apps ")
+st.title("Modenr AI Dashboard Mohamed Morgan Apps ")
 import streamlit as st
 import pandas as pd
 import numpy as np
